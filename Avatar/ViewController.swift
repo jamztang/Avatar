8076315926bb62bb1914925edf2422389f38ec3e
James Tang <james@goodnotesapp.com>
2019-07-03 16:34:47 +0800

    Add flags

--- Avatar/ViewController.swift
+++ Avatar/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+    let subject = PublishSubject<String>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+
+        #if os(UIKitForMac)
+        Swift.print("os(UIKitForMac)")
+        #endif
+
+        #if os(macOS)
+        Swift.print("os(macOS)")
+        #endif
+
+        #if os(iOS)
+        Swift.print("os(iOS)")
+        #endif
+
+        #if targetEnvironment(simulator)
+        Swift.print("targetEnvironment(simulator)")
+        #endif
+
+        #if targetEnvironment(UIKitForMac)
+        Swift.print("targetEnvironment UIKitForMac")
+        #endif
     }
-
-
 }
 
